c924a92028bdd2a5976ada4457bb36d8a5ace6bb
Adam Anklewicz <adam@anklewicz.com>
2025-07-17 14:20:30 -0400

    Keynote and setup file

---
+++ setup.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+basePath="/Users/aanklewicz/Desktop/GitHub/autoautopkgpsu2025"
+
+mkdir "$basePath/autopkg_src/"
+mkdir "$basePath/autopkg_src/overrides/"
+mkdir "$basePath/autopromote/"
+mkdir "$basePath/manifests/"
+mkdir "$basePath/pkgsinfo/"
+cp "/Users/aanklewicz/Desktop/Demo files/README.md" "$basePath/"
